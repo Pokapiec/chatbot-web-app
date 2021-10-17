2053730f8d4720f5ee97e1a30edc6ff746e4aa9c
Paweł Okapiec <pokapiec@gmail.com>
2021-10-17 12:38:40 +0200

    Reduce slug file size v2

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from transformers import pipeline, Conversation
-from prediction import question_answer, TEXT
+# from prediction import question_answer, TEXT
 from flask_cors import CORS
 
 conversational_pipeline = pipeline("conversational")
@@ -16,13 +16,13 @@ def main():
 @app.route('/get-response/')
 def message_repsonse():
     message = request.args.get('message')
-    if not message:
-        return jsonify({'answer': 'failed'})
-    answer = question_answer(message, TEXT)
-    # print(answer)
-    if answer in ('[sep]', ''):
-        answer = conversational_pipeline(Conversation(message))
-        answer = answer.generated_responses[0]
+    # if not message:
+    #     return jsonify({'answer': 'failed'})
+    # answer = question_answer(message, TEXT)
+    # # print(answer)
+    # if answer in ('[sep]', ''):
+    answer = conversational_pipeline(Conversation(message))
+    answer = answer.generated_responses[0]
     data = {'answer': answer}
     return jsonify(data)
 
