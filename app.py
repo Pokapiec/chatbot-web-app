abce7b6ef80c097cb8a88d58bdb8219a6a191b18
Paweł Okapiec <pokapiec@gmail.com>
2021-10-18 21:06:11 +0200

    Add DialoGPT

--- app.py
+++ app.py
@@ -19,7 +19,25 @@ from flask_cors import CORS
 # The University of Technology has a branch in Płock. It houses the Faculty of Civil Engineering, Mechanics and Petrochemistry, the College of Economic and Social Sciences, DS Wcześniak, and other facilities (sports, teaching, laboratories).'''
 
 # nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
+def get_answer(question):
+    # Let's chat for 5 lines
+    # for step in range(5):
+        # encode the new user input, add the eos_token and return a tensor in Pytorch
+    new_user_input_ids = tokenizer.encode(question + tokenizer.eos_token, return_tensors='pt')
+
+    # append the new user input tokens to the chat history
+    bot_input_ids = new_user_input_ids
+
+    # generated a response while limiting the total chat history to 1000 tokens, 
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+    # pretty print last ouput tokens from bot
+    return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
 app = Flask(__name__, static_url_path='', static_folder='./frontend/build')
 CORS(app, resources={r"/get-response/*": {"origins": "*"}})
@@ -36,13 +54,17 @@ def message_repsonse():
     #     return jsonify({'answer': 'failed'})
     # answer = question_answer(message, TEXT)
     # # print(answer)
+
     # if answer in ('[sep]', ''):
     # answer = conversational_pipeline(Conversation(message))
     # answer = answer.generated_responses[0]
+
     # an = nlp({"question": "What can i study there ?", 
     # "context": TEXT})
     # answer = an['answer']
-    data = {'answer': 'Hej'}
+
+    answer = get_answer(message)
+    data = {'answer': answer}
     return jsonify(data)
 
 
